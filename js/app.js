e6e6bc5b2a4667e5e0c957bb729fe67ba72a3005
Eselonn <31970222+Eselonn@users.noreply.github.com>
2017-12-20 21:54:29 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var scene = new THREE.Scene();
+			var scene = new THREE.Scene();
 			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 			
 			var renderer = new THREE.WebGLRenderer({alpha:true});
@@ -7,43 +7,60 @@ var scene = new THREE.Scene();
 			document.body.appendChild( renderer.domElement );
 			//Render object shape
 			var geometry = new THREE.BoxGeometry(1, 1, 1);
+			// 
+			// Floor
+			var floorMaterial = new THREE.MeshBasicMaterial({color: 0xff00ff,  side:THREE.DoubleSide})
+			var floorGeometry = new THREE.PlaneGeometry(10, 10, 10, 10);
+			var floor = new THREE.Mesh(floorGeometry, floorMaterial);
+			floor.position.y = -0.5;
+			floor.rotation.x = -Math.PI/2;
+			scene.add( floor );
+			// 1st wall
+			var wall1Material = new THREE.MeshBasicMaterial({color: 0x0000ff,  side:THREE.DoubleSide})
+			var wall1Geometry = new THREE.PlaneGeometry(10,10,10,10);
+			var wall1 = new THREE.Mesh(wall1Geometry, wall1Material);
+			wall1.position.y = 4.5;
+			wall1.position.z -=5;
+			wall1.rotation.x = Math.PI;
+			scene.add( wall1 );
+			// 2nd wall
+			var wall2Material = new THREE.MeshBasicMaterial({color: 0xff0000,  side:THREE.DoubleSide})
+			var wall2Geometry = new THREE.PlaneGeometry(10,10,10,10);
+			var wall2 = new THREE.Mesh(wall2Geometry, wall2Material);
+			wall2.position.y = 4.5;
+			wall2.position.x += 5;
+			wall2.rotation.y = Math.PI/2;
+			wall2.rotation.x = Math.PI;
+			scene.add( wall2 );
 			//Rotation controls
 			var controls = new THREE.OrbitControls( camera, renderer.domElement );
 			//Render material
 			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 			//Creating cube object using shape and material
 			var cube = new THREE.Mesh( geometry, material );
+			cube.position.y = 0.01;
+			cube.rotation.x = Math.PI/2;
 			//Adding cube object to scene
 			scene.add( cube );
 			camera.position.z = 5;
-			//Color palete for background;
-			window.onload = function(){
-				document.getElementById('textInput1').value=document.getElementById("r").value; 
-				document.getElementById('textInput2').value=document.getElementById("g").value; 
-				document.getElementById('textInput3').value=document.getElementById("b").value; 
-				changeBackground();
-			}; 
+
+			// GUI
+			var gui = new dat.GUI();
+			var parameters = 
+			{
+				a: '#ff8800', // color(hex)
+				b: "Tekst"
+			};
+			//
+			var controller = gui.addColor( parameters, 'a' ).name('Color');
+			gui.add(parameters , "b").name('Gaida');
+			controller.onChange(function(){
+				document.body.childNodes[8].style.backgroundColor = parameters["a"];
+			});
+			gui.open();
 			console.log(document.body.childNodes);
 			// 
-			function updateTextInput1(val) {
-          		document.getElementById('textInput1').value=val; 
-        	}
-        	// 
-        	function updateTextInput2(val) {
-          		document.getElementById('textInput2').value=val; 
-        	}
-        	// 
-        	function updateTextInput3(val) {
-          		document.getElementById('textInput3').value=val; 
-        	}
-			//
 			// 
-			function changeBackground(){
-				var r = parseInt(document.getElementById('r').value);
-				var g = parseInt(document.getElementById('g').value);
-				var b = parseInt(document.getElementById('b').value);
-				document.body.childNodes[8].style.backgroundColor = 'rgb('+[r,g,b].join(",")+')';
-			}
 			// Handling resizing by user
 			window.addEventListener("resize", function(){
 				renderer.setSize( window.innerWidth, window.innerHeight );
@@ -57,5 +74,34 @@ var scene = new THREE.Scene();
 				renderer.render( scene, camera );
 			}
 			animate();
-
 			
+			
+			function handleFileSelect(evt) {
+				var files = evt.target.files;
+				for (var i = 0, f; f = files[i]; i++) {
+				  if (!f.type.match('image.*')) {
+					continue;
+				  }
+				  var reader = new FileReader();
+				  reader.onload = (function(theFile) {
+					return function(e) {
+						scene.remove(wall1);
+						var loader = new THREE.TextureLoader();
+						loader.load(e.target.result, function ( texture ) {
+						wall1Material = new THREE.MeshBasicMaterial( {map: texture, side:THREE.DoubleSide } );
+						wall1Geometry = new THREE.PlaneGeometry(10,10,10,10);
+						wall1 = new THREE.Mesh(wall1Geometry, wall1Material);
+						wall1.position.y = 4.5;
+						wall1.position.z -=5;
+						camera.position.set( 0, 0, 5 );
+						camera.lookAt( scene.position );
+						controls.enabled = false;
+						
+						scene.add(wall1);
+						});
+					};
+				  })(f);
+				reader.readAsDataURL(f);
+				}
+			}
+			document.getElementById('imgConvert').addEventListener('change', handleFileSelect, false);
