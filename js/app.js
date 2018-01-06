a94e33aa379260cbc8f5b6d00ae0283ae84950b9
Sniadek <wojokarolg9@gmail.com>
2018-01-06 14:18:02 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,61 +1,78 @@
-var scene = new THREE.Scene();
-			var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+			class App{
+				constructor(){
+					this.guiRef = new GUI();
+					this.scene = new THREE.Scene();
+					this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
+					this.camera.position.z = 3000;
+					this.lighting = new THREE.AmbientLight(0x404040);
+					this.scene.add(this.lighting);
+					this.renderer = new THREE.WebGLRenderer({alpha:true});
+					this.loader = new THREE.TDSLoader();
+					this.materialLoader = new THREE.TextureLoader();
+					this.path = 'js/model.3ds';
+					this.geometry = new THREE.BoxGeometry(1, 1, 1);
+					this.renderer.setSize( window.innerWidth, window.innerHeight );
+					document.body.appendChild( this.renderer.domElement );
+					this.controls = new THREE.OrbitControls( this.camera, this.renderer.domElement );
+					this.animate = this.animate.bind(this);
+					this.listeners();
+					this.getMaterials();
+					this.getModel();
+					this.animate();
+					
+					
+					
+				
 			
-			var renderer = new THREE.WebGLRenderer({alpha:true});
-			renderer.setSize( window.innerWidth, window.innerHeight );
-			//Add canvas to DOM
-			document.body.appendChild( renderer.domElement );
-			//Render object shape
-			var geometry = new THREE.BoxGeometry(1, 1, 1);
-			//Rotation controls
-			var controls = new THREE.OrbitControls( camera, renderer.domElement );
-			//Render material
-			var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-			//Creating cube object using shape and material
-			var cube = new THREE.Mesh( geometry, material );
-			//Adding cube object to scene
-			scene.add( cube );
-			camera.position.z = 5;
-			//Color palete for background;
-			window.onload = function(){
-				document.getElementById('textInput1').value=document.getElementById("r").value; 
-				document.getElementById('textInput2').value=document.getElementById("g").value; 
-				document.getElementById('textInput3').value=document.getElementById("b").value; 
-				changeBackground();
-			}; 
-			console.log(document.body.childNodes);
-			// 
-			function updateTextInput1(val) {
-          		document.getElementById('textInput1').value=val; 
-        	}
-        	// 
-        	function updateTextInput2(val) {
-          		document.getElementById('textInput2').value=val; 
-        	}
-        	// 
-        	function updateTextInput3(val) {
-          		document.getElementById('textInput3').value=val; 
-        	}
-			//
-			// 
-			function changeBackground(){
-				var r = parseInt(document.getElementById('r').value);
-				var g = parseInt(document.getElementById('g').value);
-				var b = parseInt(document.getElementById('b').value);
-				document.body.childNodes[8].style.backgroundColor = 'rgb('+[r,g,b].join(",")+')';
-			}
-			// Handling resizing by user
-			window.addEventListener("resize", function(){
-				renderer.setSize( window.innerWidth, window.innerHeight );
-				camera.aspect = window.innerWidth / window.innerHeight;
-			   	camera.updateProjectionMatrix();
-			});
-			//
-			function animate() {
-				requestAnimationFrame( animate );
-				renderer.render( scene, camera );
-				renderer.render( scene, camera );
-			}
-			animate();
+				}
 
-			
+				listeners(){
+					window.addEventListener("resize", function(){
+						console.log("W: ",window.innerWidth,"H: ",window.innerHeight);
+						this.renderer.setSize( window.innerWidth, window.innerHeight );
+						this.camera.aspect = window.innerWidth / window.innerHeight;
+					   	this.camera.updateProjectionMatrix();
+					});
+
+
+				}
+
+				animate(){
+				requestAnimationFrame(this.animate);
+				this.renderer.render(this.scene,this.camera);
+				
+				
+				}
+
+				getMaterials(){
+					this.materialLoader.setPath('materialy');
+					this.materials = this.materialLoader.load('/niebieski.jpg',(texture)=>{
+							this.material = new THREE.MeshStandardMaterial({map:texture});
+							console.log(this.material);
+							
+
+					});
+				}
+
+				appendMaterial(){
+					for(let x = 0; x < this.object.children.length; x++){
+						this.object.children[x].material = this.material;
+					}
+				}
+
+				getModel(){
+					this.loader.setPath(this.path);
+					this.loader.load(this.path,(object)=>{
+					this.scene.add(object)
+					this.object = object;
+					this.appendMaterial();
+					
+					});
+
+				};
+
+				
+
+			}
+
+			const app = new App();
